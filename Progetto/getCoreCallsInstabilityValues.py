be0408261fe09321765d5181f4fde8cc616e265d
Assunta Oropallo <assunta.oropallo@gmail.com>
2017-01-17 18:47:45 +0100

    refactoring: rename

--- Progetto/getCoreCallsInstabilityValues.py
+++ Progetto/getCoreCallsInstabilityValues.py
@@ -1,4 +1,4 @@
-import parserCountChiamateClassi
+import parserCountCallsClass
 
 #funzione che date due liste restituisce tutti gli elementi presenti nella prima ma non nella seconda
 def diffClassi(first, second):
@@ -9,8 +9,8 @@ def diffClassi(first, second):
 #restituisce una array con: il numero di chiamate della versione 1,
 # il numero di chiamate della versione 2, il numero di chiamate non piu' presenti, il numero di chiamate aggiunte
 def getCoreCallsValues(path1, path2):
-    calls1= parserCountChiamateClassi.getDotFiles(path1)
-    calls2= parserCountChiamateClassi.getDotFiles(path2)
+    calls1= parserCountCallsClass.getDotFiles(path1)
+    calls2= parserCountCallsClass.getDotFiles(path2)
     old=diffClassi(calls1,calls2)
     new=diffClassi(calls2,calls1)
     return [len(calls1), len(calls2), old, new]
